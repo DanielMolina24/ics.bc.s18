1084f6935e54a5f278b70048712b7937550d8570
DanielMolina24 <dany4012@gmail.com>
2018-07-08 20:05:04 -0700

    complete

--- week4/recursion_practice/sum_of_integers.rb
+++ week4/recursion_practice/sum_of_integers.rb
@@ -1,9 +1,12 @@
 def sum_of_integers(n)
-  if ______________________
+  if n==0
     n
+  elsif n<0
+    puts 'Enter a number that is zero or greater than one'
   else
-    ______________________
+    n + sum_of_integers(n-1)
+
   end
 end
 
-puts sum_of_integers __
+puts sum_of_integers 10
