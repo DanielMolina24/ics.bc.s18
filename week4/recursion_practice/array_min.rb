1db0e46d6f49a497a2fcd3fef73bbc9f1fd2c7bd
DanielMolina24 <dany4012@gmail.com>
2018-07-08 20:04:38 -0700

    complete

--- week4/recursion_practice/array_min.rb
+++ week4/recursion_practice/array_min.rb
@@ -1,9 +1,15 @@
 def array_min(array, len)
-  if ______________________
+  if len == 1
     array[0]
   else
-    ______________________
+    first = array[0]
+    array.shift
+    rest = array_min(array, len - 1)
+    if first<rest
+      first
+    else
+      rest
+    end
   end
 end
-
-puts array_min __, __ 
+puts array_min([4, 3, 1, 17, 14, 2], 6)
