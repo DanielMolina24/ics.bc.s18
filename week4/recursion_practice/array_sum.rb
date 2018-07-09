4e3eab089b14b40af7d05b6367b0f4c033d4ec3c
DanielMolina24 <dany4012@gmail.com>
2018-07-08 20:04:44 -0700

    complete

--- week4/recursion_practice/array_sum.rb
+++ week4/recursion_practice/array_sum.rb
@@ -1,9 +1,9 @@
 def array_sum(array)
-  if ______________________
+  if array.count == 0
     array[0]
   else
-    ______________________
+    array[array.count]+array_sum(array)
   end
 end
 
-puts array_sum [__, __, ...] 
+puts array_sum [4, 5]
