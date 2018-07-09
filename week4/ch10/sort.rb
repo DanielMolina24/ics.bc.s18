fa2875f680b9783143b8ed13facca3fc81de2757
DanielMolina24 <dany4012@gmail.com>
2018-07-08 20:03:51 -0700

    complete

--- week4/ch10/sort.rb
+++ week4/ch10/sort.rb
@@ -1,3 +1,13 @@
+thing = 'word'
+words =[]
+
+puts 'Type any word and hit enter'
+puts 'When finished hit enter without saying something'
+while thing != ''
+  thing = gets.chomp
+  words = words.push thing
+end
+
 def sort(some_array) # This "wraps" recursive_sort.
   recursive_sort some_array, []
 end
