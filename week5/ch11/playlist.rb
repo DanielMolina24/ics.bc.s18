2cf314e172be2eb9e03aab6d4f896195b98f7fbb
DanielMolina24 <dany4012@gmail.com>
2018-07-14 12:07:50 -0700

    Assignment

--- week5/ch11/playlist.rb
+++ week5/ch11/playlist.rb
@@ -1,4 +1,36 @@
 ### Copy in your shuffle method first
+def shuffle array
+x = 0
+track_number = 0
+number_songs = 0
+playlist = [ ]
 
+number_songs = array.length
+
+
+while x < number_songs
+
+  track_number = rand(number_songs)
+
+  if array[track_number] != ''
+    playlist.push array[track_number]
+    array[track_number] = ''
+    x = x + 1
+  else
+  end
+end
+
+
+filename = 'Desktop/playlist.m3u'
+
+File.open filename, 'w' do |f|
+  f.puts playlist
+end
+
+end
 
 ### Now use it to build your playlist:
+
+tracks = Dir['/Users/danielmolina/Desktop/**/*{mp3}']
+
+shuffle tracks
